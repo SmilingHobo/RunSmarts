ec8c333415f316cd3aeabb67870e656cae56e403
Andrey <smilinghobo93@gmail.com>
2021-06-22 19:37:54 +0300

    web

--- src/js/script.js
+++ src/js/script.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
         speed: 1200,
         adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"></button>',
         responsive: [
             {
                 breakpoint: 992,
